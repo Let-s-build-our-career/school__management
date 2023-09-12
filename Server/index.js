98be1bf25cc93107790b2544aa76eee07cff2a47
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-09-13 02:30:20 +0600

    initially server setup

--- Server/index.js
+++ Server/index.js
@@ -2,4 +2,20 @@
 //  i am tahazzat
 // I am Abu Bakar
 
+const express = require("express");
+const cors = require("cors")
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Server is ready for providing data!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

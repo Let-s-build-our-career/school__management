3c55afd5dff918936b63bdb38d996af91bce5d87
developer-shourav <mrinmoyraj823@gmail.com>
2023-09-14 19:34:24 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div className="h-screen max-w-[90rem] mx-auto px-2 md:px-4 lg:px-0">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
